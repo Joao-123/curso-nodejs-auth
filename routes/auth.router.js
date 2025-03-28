383db0413481c6ffb7eabcc73f21fe5af23958e1
Joao Meneces <charlymeneses7@gmail.com>
2025-03-28 11:57:41 -0400

    clase 16 (refactor and send mail /recovery)

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const { config } = require('../config/config')
+const AuthService = require('../services/auth.service')
 
 const router = express.Router();
+const service = new AuthService();
 
 router.post('/login',
   passport.authenticate('local', {session: false}),
   async (req, res, next) => {
   try {
-    const secret = config.jwtSecret;
     const user = req.user;
-    const payload = {
-      sub: user.id,
-      role: user.role,
-    }
-    const token = jwt.sign(payload, secret);
-    res.json({
-      user,
-      token,
-    });
+    res.json(service.signToken(user));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/recovery',
+  async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const rta = await service.sendMail(email);
+    res.json(rta);
   } catch (error) {
     next(error);
   }
